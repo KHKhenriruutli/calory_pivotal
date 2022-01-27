394c209a5e401cd6d4b6ae4bec1b117010eff299
KHKHenriRuutli <henri.ruutli@khk.ee>
2022-01-27 12:54:34 +0200

    Added ToiduLisamiseFUnktsionaalsus [Delivers #180976653]

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
 
+const StorageCtrl = (function () {
+    return {
+        storeItem: function (item) {
+            let items;
+            if (localStorage.getItem("items") === null) {
+                items = []
+                items.push(item)
+                localStorage.setItem("items", JSON.stringify(items))
+
+            }
+            else {
+                items = JSON.parse(localStorage.getItem("items"))
+                items.push(item)
+                localStorage.setItem("items", JSON.stringify(items))
+            }
+        },
+
+        getItemsFromStorage: function () {
+            let items;
+            if (localStorage.getItem("items") === null) {
+                items = []
+
+            } else {
+                items = JSON.parse(localStorage.getItem("items"))
+            }
+            return items;
+        }
+    }
+
+})();
 
 const itemCtrl = (function() {
+    const item = function(id, name, calories) {
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
 
     const data = {
         items: [
@@ -14,7 +49,35 @@ const itemCtrl = (function() {
         getitems: function() {
             return data.items;
         },
-
+        logData: function() {
+            return data
+        },
+        addItem: function(name, calories) {
+            console.log(name);
+            console.log(calories);
+            let ID;
+            if(data.items.length > 0) {
+                ID = data.items[data.items.length - 1].id + 1
+                console.log(ID);
+            }
+            else {
+                ID = 0
+            }
+            calories = parseInt(calories);
+            let newItem = new item(ID, name, calories);
+            data.items.push(newItem);
+            console.log(data.items);
+            console.log(newItem);
+            return newItem;
+        },
+        getTotalCalories: function() {
+            let total = 0;
+            data.items.forEach(function(item) {
+                total += item.calories;
+                console.log(total);
+            });
+            return total;
+        }
     }
 } ) ();
 
@@ -39,8 +102,33 @@ const UICtrl = (function() {
             });
             document.querySelector(UISelectors.itemlist).innerHTML = html;
         },
+        getSelectors: function() {
+            return UISelectors;
+        },
+        getItemInput: function() {
+            return {
+                name: document.querySelector(UISelectors.itemNameInput).value,
+                calories: document.querySelector(UISelectors.itemCaloriesInput).value
+            }
+        },
+        addListItem: function(item) {
+            const li = document.createElement("li");
+            li.className = "collection-item";
+            li.id = `item ${item.id}`;
+            li.innerHTML = `<strong> ${item.name} :</strong> <em> ${item.calories} Calories </em>
+            <a href="#" class="secondary-content"> <i class="edit-item fa fa-pencil"> </i></a>`;
+            console.log(li);
 
+            document.querySelector(UISelectors.itemlist).insertAdjacentElement("beforeend", li);
 
+        },
+        clearInput: function() {
+            document.querySelector(UISelectors.itemNameInput).value = "";
+            document.querySelector(UISelectors.itemCaloriesInput).value = "";
+        },
+        showTotalCalories: function(totalCalories) {
+            document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
+        }
 
     }
 }) ();
@@ -48,9 +136,39 @@ const UICtrl = (function() {
 
 const App = (function(itemCtrl, UICtrl, StorageCtrl) {
     const loadEventListeners = function() {
+        console.log("event loaders loading")
+        const UISelectors = UICtrl.getSelectors();
+        console.log(UISelectors);
+        document.querySelector(UISelectors.addBtn).addEventListener("click", itemAddSubmit);
+        document.addEventListener("DOMContentLoaded", getItemsFromStorage)
+    }
+    const itemAddSubmit = function(event) {
+        console.log("itemaddeventfunction");
+        const input= UICtrl.getItemInput();
+        if(input.name !== "" && input.calories !== "") {
+            const newItem = itemCtrl.addItem(input.name, input.calories);
+            console.log(newItem);
+            UICtrl.addListItem(newItem);
+            const totalCalories = itemCtrl.getTotalCalories();
+            UICtrl.showTotalCalories(totalCalories);
+            StorageCtrl.storeItem(newItem)
+            UICtrl.clearInput();
 
+        }
+        event.preventDefault();
     }
 
+    const getItemsFromStorage = function() {
+        const items = StorageCtrl.getItemsFromStorage()
+        items.forEach(function(item) {
+            itemCtrl.addItem(item["name"], item["calories"])
+        })
+
+        const totalCalories = itemCtrl.getTotalCalories();
+        UICtrl.showTotalCalories(totalCalories);
+        UICtrl.populateItemList(items);
+
+    }
 
     return {
         init : function() {
@@ -63,7 +181,7 @@ const App = (function(itemCtrl, UICtrl, StorageCtrl) {
 
         }
     }
-}) (itemCtrl, UICtrl);
+}) (itemCtrl, UICtrl, StorageCtrl);
 
 
 
